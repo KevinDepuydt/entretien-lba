0fc0a08e1ba38e4713b4b22a20280eb1968f6d48
Kevin Depuydt <kevin.dpdt@gmail.com>
2023-06-07 10:46:54 +0200

    fetch data from server

--- entretien/server.js
+++ entretien/server.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const https = require("https");
+// const https = require("https");
 const express = require("express");
 /**
  * Format de la réponse JSON
@@ -20,20 +20,11 @@ const express = require("express");
   })
 
   app.get("/data", async (req, res) => {
-    let organismes = getData1();
-
-    const formations = await getData2();
-
-    let apprentis = null;
-    getData3((data) => {
-      apprentis = data;
-    });
-
-    return res.json({
-      organismes,
-      formations: formations,
-      apprentis,
-    });
+    // fetch organismes
+    const organismes = await getData3();
+    // map organismes to get only their name
+    const data = organismes.map(({ nom }) => ({ nom }));
+    return res.json(data);
   });
 
   app.listen(3000, () => console.log(`Server ready and listening on port ${3000}`));
@@ -45,39 +36,56 @@ const express = require("express");
  * @returns {Promise<*>}
  */
 async function getData1() {
-  let response = await axios.get("https://mocki.io/v1/cbbd831b-199c-4e48-b426-1ce8ddbf1aa5");
-  let organismes = response.data;
+  try {
+    let { data: organismes } = await axios.get("https://mocki.io/v1/cbbd831b-199c-4e48-b426-1ce8ddbf1aa5");
 
-  for (let i = 0; i < organismes.length; i++) {
-    organismes[i].nom = organismes[i].raison_sociale;
-    delete organismes[i].raison_sociale;
+    for (let i = 0; i < organismes.length; i++) {
+      organismes[i].nom = organismes[i].raison_sociale;
+      delete organismes[i].raison_sociale;
+    }
+
+    return organismes;
+  } catch (e) {
+    return [];
   }
-
-  return organismes;
 }
 
 async function getData2() {
   try {
-    return axios.get("https://mocki.io/v1/cbbd831b-199c-4e48-b426-1ce8ddbf1aa5").then((response) => response.data);
+    const { data } = await axios.get("https://mocki.io/v1/cbbd831b-199c-4e48-b426-1ce8ddbf1aa5");
+    console.log('getData2', data);
+    return data;
   } catch (e) {
     console.error(e);
+    return [];
   }
 }
 
-const getData3 = (callback) => {
-  https
-    .get("https://mocki.io/v1/cbbd831b-199c-4e48-b426-1ce8ddbf1aa5", (resp) => {
-      let data = "";
-
-      resp.on("data", (chunk) => {
-        data += chunk;
-      });
-
-      resp.on("end", () => {
-        return callback(data);
-      });
-    })
-    .on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
+const getData3 = async () => {
+  try {
+    const { data } = await axios.get("https://mocki.io/v1/cbbd831b-199c-4e48-b426-1ce8ddbf1aa5")
+    console.log('getData3', data);
+    return data;
+  } catch (e) {
+    console.log("getData3 error: ", e.message);
+    return [];
+  }
 };
+
+// const getData3 = (callback) => {
+//   https
+//     .get("https://mocki.io/v1/cbbd831b-199c-4e48-b426-1ce8ddbf1aa5", (resp) => {
+//       let data = "";
+//
+//       resp.on("data", (chunk) => {
+//         data += chunk;
+//       });
+//
+//       resp.on("end", () => {
+//         return callback(data);
+//       });
+//     })
+//     .on("error", (err) => {
+//       console.log("Error: " + err.message);
+//     });
+// };
